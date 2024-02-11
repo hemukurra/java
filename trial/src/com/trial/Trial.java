36aaead164d20d73be94dd49d07fc6d062799910
Hemu <Hemu@DESKTOP-KRG1IRK.mynetworksettings.com>
2024-02-11 03:59:41 -0500

    first commit

---
+++ trial/src/com/trial/Trial.java
@@ -0,0 +1,18 @@
+package com.trial;
+
+public class Trial {
+
+	public static void main(String[] args) {
+		int a=10;
+		boolean b= true;
+		if(a==16) {
+			System.out.println(b);
+		}
+		else {
+			System.out.println("error");
+		}
+	}
+
+	}
+
+
